09d4e895c66925b4ea8c46579bbc354a38225dc2
! | EkoT <75649447+iIEkoT@users.noreply.github.com>
2021-04-29 15:19:08 +0300

    functions
    Definition of functions

---
+++ index.js
@@ -0,0 +1,53 @@
+//const every:
+const endscript = "script"; // Connect everything ends with script
+
+const jsprefix = ".js"; // Connect everything ends with javascript prefix
+
+let pyscript = ".py"; // Connect python prefix to py prefix
+
+const file = 'index';
+
+let lockscript = function lockscript() {
+  `${async = file}, import, ${lockscript}`
+};
+
+let asyncScript = console.log(
+  console.log(`${lockscript}`),
+  console.delete
+);
+
+let python = "python";
+
+python.endscript = asyncScript + endscript;
+
+let printscript = `${asyncScript}` || "function"
+
+let print = function printscript() {
+  console.log(`${asyncScript}`);
+}
+
+console.log([lockscript => { 1 + 1 }], [printscript], `function`);
+
+let direct = console.delete;
+
+let imoprt = `${async = print}` + endscript;
+
+let join = imoprt;
+
+join.asyncScript + printscript;
+
+imoprt.report = {
+  asyncScript
+};
+
+eval = asyncScript;
+
+file.script = asyncScript = console.delete;
+
+let infile = "inside of file:";
+
+let outfile = "outside of file:";
+
+let ftype = infile || outfile || file;
+
+ftype.repeat.join;
