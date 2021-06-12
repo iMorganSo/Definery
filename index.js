192e799799f8054069e73b5629bfcb04763401c6
! | EkoT <75649447+iIEkoT@users.noreply.github.com>
2021-06-13 01:20:20 +0300

    definer

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const ablover = function xmath() {
   x = Math.floor[(1 + 2*(0 * 3))]
 }
 
-var definer = new define(
+var leaterrr = new define(
   define(new function def() {
     ablover(define(1 + 2*(4 + 1) || x * 3))
   })
